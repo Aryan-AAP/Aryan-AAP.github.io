bdafe9f4370d5cd532a81bfdf51e749e4233161d
Aryan-AAP <pardeshiaryanon@gmail.com>
2024-06-15 18:03:29 +0530

    lol just added and remove centry and post hog

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://8e8a395edab9c1f8cf8e54e51b812b72@o4506932126154752.ingest.us.sentry.io/4507435606081536",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
